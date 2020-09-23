f2bdbec226ef6f07149e419c5b67324c86806b03
pumpkinetcher <71747085+pumpkinetcher@users.noreply.github.com>
2020-09-23 10:33:33 -0700

    1st Program
    I will update this with new coding skillz as I learn them to make it more complex.

---
+++ Practice.py
@@ -0,0 +1,38 @@
+
+print("Hello there! Do you want to play a game?")
+
+print("//////////////////////////////")
+
+the_only_right_answer = input("Type -Yes- to make the code make sense: ")
+
+print("If you answered with -Yes- like you should've (because you follow instructions), then hooray!")
+print("If you typed -No- well too bad you are already here you might as well play!")
+
+print("//////////////////////////////")
+
+print("It's time for a Mad Lib! Fill in the blanks with the type of word being asked for.")
+
+input("Get on with it already! (Type -Ok-): ")
+
+print("//////////////////////////////")
+
+
+print("I took Sparky for a _____.")
+print("_____ told her that she did _____")
+print("She ______ her ______ in delight")
+
+
+print("//////////////////////////////")
+
+
+
+verb = input("Type any verb: ")
+noun1 = input("Type any noun: ")
+adjective = input("Type any adjective: ")
+verb2 = input("Type any past-tense verb: ")
+dog_body_part = input("Pick a dog's body part: ")
+
+
+print("I took Sparky for a " + verb)
+print(noun1 + " told her that she did " + adjective)
+print("She " + verb2 + " her " + dog_body_part + " in delight")
